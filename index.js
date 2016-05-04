d074ea4290adfe3645848627c6bf52c12b3d304f
Mike Walters <mike@eventbooking.com>
2016-05-04 13:34:12 -0400

    added styles

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 var conversion = require("phantom-html-to-pdf")(),
     phantomjs = require("phantomjs-prebuilt"),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    fs = require("fs");
+
+function getHtml($, $styles, section) {
+    var $view = $('<div class="fr-view"></div>');
+    var $section = $(section);
+    $view.append($styles);
+    $view.append($section);
+    return $.html($view);
+}
 
 function convert(html, cb) {
     var $ = cheerio.load(html);
 
-    var options = {
-        header: $('header').html(),
-        html: $('content').html(),
-        footer: $('footer').html(),
-        phantomPath: phantomjs.path
-    }
+    fs.readFile('./bower_components/froala-wysiwyg-editor/css/froala_style.css', 'utf8', function (err, css) {
+        var $styles = $('<style type="text/css"></style>').text(css);
 
-    conversion(options, function (err, pdf) {
-        if (cb) {
-            cb(err, pdf);
+        var options = {
+            header: getHtml($, $styles, 'header'),
+            html: getHtml($, $styles, 'content'),
+            footer: getHtml($, $styles, 'footer'),
+            phantomPath: phantomjs.path
         }
-        conversion.kill();
+
+        conversion(options, function (err, pdf) {
+            if (cb) {
+                cb(err, pdf);
+            }
+            conversion.kill();
+
+        });
     });
 }
 

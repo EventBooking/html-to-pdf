402c5234ce133dcc4c61ec201c93b2a3641e4c84
Mike Walters <mike@eventbooking.com>
2016-09-14 17:20:42 -0400

    switched render engines.... added docker

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:4.3
+
+MAINTAINER Michael Walters, mike@eventbooking.com
+
+RUN apt-get update
+RUN apt-get install zip -y
+
+WORKDIR /home
+
+RUN echo '{ "allow_root": true }' > /root/.bowerrc
+RUN npm install -g bower
+
+ADD package.json package.json
+RUN npm install
+
+ADD ./bin/wkhtmltopdf /home/bin/wkhtmltopdf
+RUN chmod +x /home/bin/wkhtmltopdf
+
+ADD index.js index.js
+ADD styles.css styles.css
--- index.js
+++ index.js
@@ -1,63 +1,100 @@
-var conversion = require("phantom-html-to-pdf")(),
-    phantomjs = require("phantomjs-prebuilt"),
+var wkhtmltopdf = require("wkhtmltopdf"),
     cheerio = require('cheerio'),
-    fs = require("fs"),
-    minify = require("html-minifier").minify,
-    strip = require("strip-comment");
+    fs = require("fs");
 
-function getBuffer(stream, callback) {
-    var buffer;
-    stream.on('data', function (data) {
-        buffer = data;
+wkhtmltopdf.command = "./bin/wkhtmltopdf";
+
+function getBuffer(stream) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(name, content, error => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(name);
+        });
     });
-    stream.on('end', function () {
-        callback(buffer);
+}
+
+function readFile(name, type) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(name, type, (error, content) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(content);
+        });
+    });
+}
+
+function writeFile(name, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(name, content, error => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(name);
+        });
+    });
+}
+
+function render(content, options) {
+    return new Promise((resolve, reject) => {
+        wkhtmltopdf(content, options, (error, stream) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            var chunks = [];
+            stream.on('data', data => {
+                chunks.push(data);
+            });
+
+            stream.on('end', () => {
+                var buffer = Buffer.concat(chunks);
+                resolve(buffer);
+            });
+
+        });
     });
 }
 
 function fixImport(html) {
     
-    html = html.replace('url("//', 'url("');
+    html = html.replace('url("//', 'url("http://');
     return html;
 }
 
-function sanitize(html) {
-    if (html == null)
-        return null;
-
-    html = fixImport(html);
-
-    var $ = cheerio.load(html);
-    
-    var $head = $('head');
-    $head.prepend('<style type="text/css">html{zoom: 0.53;}</style>');
-    
-    return $.html();
-}
-
 exports.convert = function (event, context, callback) {
+    // ToDo: header/footer
 
-    event.header = sanitize(event.header);
-    event.html = sanitize(event.html);
-    event.footer = sanitize(event.footer);
-    event.phantomPath = phantomjs.path;
+    // backward compat with original phantom render engine
+    var paperSize = event.paperSize || {};
 
-    conversion(event, function (err, pdf) {
-        if (err) {
-            console.log(err);
-            callback(err);
-            return;
-        }
+    var options = {
+        marginLeft: "10mm",
+        marginRight: "10mm",
+        orientation: paperSize.orientation
+    };
 
-        getBuffer(pdf.stream, function (buffer) {
-            if (callback) {
-                var base64 = buffer.toString('base64');
-                callback(err, {
-                    data: base64
-                });
-            }
-        })
+    var $styles;
 
-        conversion.kill();
+    Promise.all([
+        readFile('styles.css', 'utf-8')
+    ]).then( styles => {
+        // ToDo: do something with the styles
+        var $ = cheerio.load(event.html);
+        var $styles = $('<style type="text/css"></style>').text(styles.join(';'));
+        $("head").append($styles);
+        var html = $.html();
+        return render(fixImport(html), options);
+    }).then(buffer => {
+        var base64 = buffer.toString('base64');
+        callback(null, { data: base64 });
+    }).catch(error => {
+        callback(error);
     });
 }

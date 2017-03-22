ae615e5857e4522697e88edee78ac35160476bdf
Jordan Brown <jordan.brown@eventbooking.com>
2017-03-22 16:44:42 -0400

    first cut of express changes

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var wkhtmltopdf = require("wkhtmltopdf"),
+var express = require("express"),
+    bodyParser = require("body-parser")
+    wkhtmltopdf = require("wkhtmltopdf"),
     cheerio = require('cheerio'),
     fs = require("fs");
 
 wkhtmltopdf.command = "./bin/wkhtmltopdf";
 
-function getBuffer(stream) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(name, content, error => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            resolve(name);
-        });
+var app = express();
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.post('/', function(req, res) {
+    convert(req.body.html, req.body.args).then(result => {
+        res.send({ data: result });
+    }).catch(result => {
+        res.status(500);
+        res.send();
     });
-}
+});
+
+var server = app.listen(80);
 
 function readFile(name, type) {
     return new Promise((resolve, reject) => {
@@ -28,73 +33,48 @@ function readFile(name, type) {
     });
 }
 
-function writeFile(name, content) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(name, content, error => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            resolve(name);
-        });
-    });
-}
-
 function render(content, options) {
     return new Promise((resolve, reject) => {
-        wkhtmltopdf(content, options, (error, stream) => {
-            if (error) {
-                reject(error);
-                return;
-            }
+        var stream = wkhtmltopdf(content, options);
 
-            var chunks = [];
-            stream.on('data', data => {
-                chunks.push(data);
-            });
+        var chunks = [];
+        stream.on('data', data => {
+            chunks.push(data);
+        });
 
-            stream.on('end', () => {
-                var buffer = Buffer.concat(chunks);
-                resolve(buffer);
-            });
-
+        stream.on('end', () => {
+            var buffer = Buffer.concat(chunks);
+            resolve(buffer);
         });
     });
 }
 
 function fixImport(html) {
-    
     html = html.replace('url("//', 'url("http://');
     return html;
 }
 
-exports.convert = function (event, context, callback) {
+function convert(html, args) {
     // ToDo: header/footer
 
     // backward compat with original phantom render engine
-    var paperSize = event.paperSize || {};
-
-    var options = {
-        marginLeft: "10mm",
-        marginRight: "10mm",
-        orientation: paperSize.orientation
-    };
-
     var $styles;
 
-    Promise.all([
-        readFile('styles.css', 'utf-8')
-    ]).then( styles => {
-        // ToDo: do something with the styles
-        var $ = cheerio.load(event.html);
-        var $styles = $('<style type="text/css"></style>').text(styles.join(';'));
-        $("head").append($styles);
-        var html = $.html();
-        return render(fixImport(html), options);
-    }).then(buffer => {
-        var base64 = buffer.toString('base64');
-        callback(null, { data: base64 });
-    }).catch(error => {
-        callback(error);
+    return new Promise((resolve, reject) => {        
+        Promise.all([
+            readFile('styles.css', 'utf-8')
+        ]).then(styles => {
+            // ToDo: do something with the styles
+            var $ = cheerio.load(html);
+            var $styles = $('<style type="text/css"></style>').text(styles.join(';'));
+            $("head").append($styles);
+            var htmlStr = $.html();
+            return render(fixImport(htmlStr), args);
+        }).then(buffer => {
+            var base64 = buffer.toString('base64');
+            resolve(base64);
+        }).catch(error => {
+            reject(error);
+        });
     });
 }

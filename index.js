c64e8243c8777008469d6e58ef3458f8025b732f
Jordan Brown <jordan.brown@eventbooking.com>
2017-03-24 10:57:02 -0400

    performance tweaks

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.post('/', function(req, res) {
+    res.setTimeout(0);
+
     convert(req.body.html, req.body.args).then(result => {
         res.send({ data: result });
     }).catch(result => {

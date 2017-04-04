554979f73ad63097617fcdc1e01a51f05aca16aa
Mike Walters <mike@eventbooking.com>
2017-04-04 15:26:14 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Building docker environment
+docker-compose build
+
+# Starting docker environment
+docker-compose run --service-ports start
+
+# Running test
+npm test
+
+# Debugging
+npm run debug
+...then attach vscode debugger
--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,5 @@
-const fs = require('fs'),
-    path = require('path')
+const path = require('path')
     exec = require('child_process').exec;
 
 var wkhtmltopdf = path.join(__dirname, "./bin/wkhtmltopdf");
-//fs.chownSync(wkhtmltopdf, 0111, 0000);
 exec(`chmod +x ${wkhtmltopdf}`);
